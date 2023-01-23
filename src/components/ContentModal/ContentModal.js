d7cbe970463cebae80a430e2237430f2853cb2a5
HarisMashhood <harismashhood36@gmail.com>
2023-01-24 01:10:38 +0530

    fourth

--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
+import Modal from '@mui/material/Modal';
+import Backdrop from '@mui/material/Backdrop';
+import Fade from '@mui/material/Fade';
 import axios from "axios";
 import {
     img_500,
@@ -10,8 +10,8 @@ import {
     unavailableLandscape,
 } from "../../config/config";
 import "./ContentModal.css";
-import { Button } from "@material-ui/core";
-import YouTubeIcon from "@material-ui/icons/YouTube";
+import Button from '@mui/material/Button';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const useStyles = makeStyles((theme) => ({
     modal: {
